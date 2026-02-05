6dc6ff2e889fa98a6309f0e0745fbaa4e918146d
Bebbi <bebymaharani2@gmail.com>
2026-02-05 08:16:48 +0800

    first commit

--- home.php
+++ home.php
@@ -91,5 +91,27 @@
                                 </div>
                             </div>
                         </div>
+
+<div class="card mb-4">
+    <div class card-body>
+        <table class="table table-bordered">
+            <tr>
+                <td width="150"><strong>Nama</strong></td>
+                <td width="1">:</td>
+                <td width="200"><<?=$_SESSION['user']['nama']; ?></td>
+            </tr>
+            <tr>
+                <td width ="150"><strong> Level user</strong></td>
+                <td width ="1">:</td>
+                <td><?width =$_SESSION['user']['level']; ?></td>
+            </tr>
+            <tr>
+                <td width ="150"><strong>Tanggal Login</stong></td>
+                <td width="1">:</td>
+                <td><?= data('d-m-y')?></td>
+            </tr>
+        </table>
+    </div>
+</div>
 </body>
 </html>
